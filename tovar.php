98e221b0854960f4bb8a54e2e7b5337593a9ac8c
igkriv <ig.kriv@gmail.com>
2019-05-02 20:00:35 +0300

    PHP file created to read JSON and convert to array

--- tovar.php
+++ tovar.php
@@ -1,5 +1,16 @@
 <?php
-// create php file for one spare part
-$strJson = file_get_contents("xerox.json");
-echo $strJson;
+    // if( $_GET["pn"] ) {
+    //     echo "you get a pn - ". $_GET['pn']. "<br />";
+    //     exit();
+    // }
+    $pn = "801K24091";
+    var_dump($pn);
+    // Get the contents of the JSON file 
+    $strJson = file_get_contents("xerox.json");
+    $array = json_decode($strJson, true);
+    
+    $key = array_search($pn, $array); 
+    print_r($key); // show contents
+
+    
 ?>
